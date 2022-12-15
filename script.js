1cd3eb32740bccc920b772b23dae41e91c2e1c60
Focrult <focrult@gmail.com>
2022-12-15 14:48:10 +0800

    The criteria state for each individual button. Before when you clicked it applied the save to all changes. So I made each button seperate from one another

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-const hour = dayjs().hour();
-const DMY = dayjs().format("dddd, MMMM YYYY"); //id current day in html
-$("#currentDay").text(DMY)
+const hour = dayjs().hour(); //display the hour
+const DMY = dayjs().format("dddd, MMMM YYYY"); //Displays the day, the month, and the year
+$("#currentDay").text(DMY);
 $(document).ready(function() {
 
 //anything involving time, contain in function
 function Time() {
-console.log(hour)
 //try to parse int - converting the string to a number and compare to hour
 $(".time-block").each(function () { //go through each row with loop!
-  var rowTime = parseInt($(this).attr("id")) //applies to all rows target the ID
+  var rowTime = parseInt($(this).attr("id")); //applies to all rows target the ID
   if(rowTime > hour){ //remove any previous classes, then add new class
-    $(this).removeClass("present") //comments say use class past, present and future
-    $(this).removeClass("part")
-    $(this).addClass("future")
+    $(this).removeClass("present"); //comments say use class past, present and future
+    $(this).removeClass("part");
+    $(this).addClass("future");
   }
   if(rowTime === hour){//remove any previous classes, then add new class
-    $(this).removeClass("past")
-    $(this).removeClass("future")
-    $(this).addClass("present")
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
   }
   if(rowTime < hour){//remove any previous classes, then add new class
-    $(this).removeClass("future")
-    $(this).removeClass("present")
-    $(this).addClass("past")
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
   }
 })
 }
 //save each description
-$(".saveBtn").on("click", function(){
-localStorage.setItem("9", $(".description9").val());
-localStorage.setItem("10", $(".description10").val());
-localStorage.setItem("11", $(".description11").val());
-localStorage.setItem("12", $(".description12").val());
-localStorage.setItem("13", $(".description13").val());
-localStorage.setItem("14", $(".description14").val());
-localStorage.setItem("15", $(".description15").val());
-localStorage.setItem("16", $(".description16").val());
-localStorage.setItem("17", $(".description17").val());
+  $(".saveBtn9").on("click", function(){ 
+    localStorage.setItem("9", $(".description9").val());
+    console.log("passing through button9");
+})
+  $(".saveBtn10").on("click", function(){ 
+    localStorage.setItem("10", $(".description10").val());
+})
+  $(".saveBtn11").on("click", function(){ 
+    localStorage.setItem("11", $(".description11").val());
+})
+  $(".saveBtn12").on("click", function(){ 
+    localStorage.setItem("12", $(".description12").val());
+})
+  $(".saveBtn13").on("click", function(){ 
+    localStorage.setItem("13", $(".description13").val());
+})
+  $(".saveBtn14").on("click", function(){ 
+    localStorage.setItem("14", $(".description14").val());
+})
+  $(".saveBtn15").on("click", function(){ 
+    localStorage.setItem("15", $(".description15").val());
+})
+  $(".saveBtn16").on("click", function(){ 
+    localStorage.setItem("16", $(".description16").val());
+})
+  $(".saveBtn17").on("click", function(){ 
+    localStorage.setItem("17", $(".description17").val());
 })
 
 $(".description9").val(localStorage.getItem("9"));
